aa3a89fd96c533181fb5414b8b5e54f27f9ec0bf
AGUIRI <rayyane.aguiri@etud.univ-jfc.fr>
2020-04-28 23:29:43 -0400

    First Commit install Firebase

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAssjZY6Hb2Mby-XTlty0rsk_2OyP3D0aE",
+    authDomain: "project-4-rayyane.firebaseapp.com",
+    databaseURL: "https://project-4-rayyane.firebaseio.com",
+}
+
+export default config;
